2062a2c35a9607022849e69bbb8685a045883a1c
rafaelmiy <rafaelmiy@gmail.com>
2017-10-17 22:37:24 -0200

    Update index.js

--- resist/js/index.js
+++ resist/js/index.js
@@ -54,7 +54,7 @@
       // very wide, but not very tall
       this.font = '900 ' + this.textSize + 'px "Orbitron"';
       this.context.font = this.font;
-      this.text = "RESIST";
+      this.text = "FAMILY INC";
       this.textWidth = (this.context.measureText(this.text)).width;
 
       this.fps = 60;
